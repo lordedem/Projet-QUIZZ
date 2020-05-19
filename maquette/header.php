60675a71f12d129b4e05d2c4eb1d70955fa1c6e8
lordedem <65088542+lordedem@users.noreply.github.com>
2020-05-19 08:35:31 +0000

    Add files via upload

---
+++ maquette/header.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aclonica">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
+    
+</head>
+<body style="">
+<style>
+body {
+  background-repeat: no-repeat;
+}
+body{
+    padding: 0;
+    margin: 0;
+    background-image: url('img/img-bg.png');
+    background-size: cover;
+    background-repeat: no-repeat
+}
+.logo img{
+    height: 75px;
+    margin:0px -13px 0px 16px;
+}
+.nav{
+    background-color: #042425;
+    height: 80px;
+}
+.titre{
+    text-align: center;
+    color: white;
+    font-size: 39px;
+    margin:6px 0px 0px 361px;
+}
+
+/*Media Query*/
+@media screen and (min-width: 667px) and  (max-width: 1024px){
+    body{
+        background-size: 190%;
+    }
+    .titre{
+        margin-top: -6%;
+    }
+}
+
+@media screen and (min-width: 320px) and  (max-width: 667px){
+    body{
+        background-size: 280%;
+    }
+
+    .titre{
+        margin-top: -11%;
+        font-size: 1.5em;
+    }
+}
+#brand-logo {
+  font-size: 34px;
+}
+
+#app-navbar {
+  background: linear-gradient(120deg, #00e4d0, #5983e8);
+}
+</style>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<header>
+    <nav class="nav">
+
+       <div class="logo"><img src="img/logo-QuizzSA.png"></div>
+        <p class="titre">Le plaisir de jouer</p>
+
+    </nav>
+</header>
